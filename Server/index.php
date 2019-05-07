86fb111826018f2a2b6dfbccb7a9a98167a340e0
joniko <joni.kokko.metropolia.fi>
2019-05-07 11:12:01 +0300

    Päivitetty kyselyiden pohjat, nämä toimivia

--- Server/index.php
+++ Server/index.php
@@ -1,11 +1,6 @@
 <?php
 
 
-$servername = "localhost";
-$username = "ryhma4";
-$password = "passu";
-$dbname = "kirjasto";
-
 
 //function bookList($kid, $nimi, $kustantaja, $jvuosi, $kirjailija, $ISBN, $painos, $kieli){
     $servername = "localhost";
@@ -23,7 +18,7 @@ $dbname = "kirjasto";
     }
     echo "Connected successfully";
 
-/*
+/*  ei käytössä
     $sql = "INSERT INTO kirjat (Kirja_Id, Nimi, Kustantaja, Julkaisuvuosi, Kirjailija, ISBN, Painos, Kieli, Saatavuus)
     VALUES (004, 'Testi3', 'OTAVA', 2006, 'Kirjailija_o', '4ANa222', 3, 'Saksa','Kyllä')";
 
@@ -34,30 +29,34 @@ $dbname = "kirjasto";
 }
 $connection->close();
 */
-   /* $saatavuus = "Kyllä";
-    $kid = 001;
+
+    //Tämä toimii
+    $saatavuus = "Kyllä";
+    $kid = 004;
     $nimi = "Testi";
-    $kustantaja = null;
-    $jvuosi = null;
-    $kirjailija = null;
-    $ISBN = null;
-    $painos = null;
-    $kieli = null;
+    $kustantaja = "Oma kustanne";
+    $jvuosi = 2002;
+    $kirjailija = "kirjailija";
+    $ISBN = "ABC123ABC";
+    $painos = 1;
+    $kieli = "Ranska";
 
 
 /*
-    //Haku toimivuus epäselvä, ei toimi
-    $stmt = $connection->prepare("SELECT * FROM kirat
-    WHERE Kirja_id=? or Nimi=? or  kustantaja=? or julkaisuvuosi=? or kirjailija=? or ISBN=? or Painos=? or kieli=? and saatavuus=?");
-    $stmt->bind_param("issississ", $kid, $nimi, $kustantaja, $jvuosi, $kirjailija, $ISBN, $painos, $kieli, $saatavuus);
+    //Pätkä toimii tarvitsee vielä kimpassa miettii toteutusta haku ei liian sepsifoiva
+    $sql ="SELECT * FROM kirjat
+    WHERE Kirja_Id=? or Nimi=? or  Kustantaja=? or Julkaisuvuosi=? or Kirjailija=? or ISBN=? or Painos=? or Kieli=? and Saatavuus=?";
+   $stmt = $connection->prepare($sql);
+   $stmt->bind_param("issississ", $kid, $nimi, $kustantaja, $jvuosi, $kirjailija, $ISBN, $painos, $kieli, $saatavuus);
 
-    $result = $stmt->execute();
+   $boolean =$stmt->execute();
+   $result = $stmt->get_result();
 
-    if ($result > 0) {
+    if ($boolean > 0) {
         // output data of each row
         $kirjaArray = array();
         while($row = $result->fetch_assoc()) {
-            echo '<br />'.$row['Kirja_id'] . '<br />';
+            echo '<br />'.$row['Kirja_Id'] . '<br />';
             echo $row['Nimi'] . '<br />';
             echo $row['Kustantaja'].'<br />';
             echo $row['Julkaisuvuosi'].'<br />';
@@ -69,21 +68,53 @@ $connection->close();
             $kirjaArray[] = $row;
         }
     } else {
-        echo "0 results";
+        echo '<br />'."0 results";
     }
     $tulosArray = array('numOfRows'=>strval($result->num_rows),'rows'=>$kirjaArray);
 
     $jsonArray = json_encode($tulosArray);
     echo $jsonArray;
     $stmt->close();
+    $connection->close();
+*/
+
+/*
+   //kirjan lisääminen toimii
+    $sql = "INSERT INTO kirjat (Kirja_Id, Nimi, Kustantaja, Julkaisuvuosi, Kirjailija, ISBN, Painos, Kieli, Saatavuus)
+    Values (?,?,?,?,?,?,?,?,?)";
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param("issississ", $kid, $nimi, $kustantaja, $jvuosi, $kirjailija, $ISBN, $painos, $kieli, $saatavuus);
+    $boolean =$stmt->execute();
+    if($boolean > 0){
+        echo '<br />'."Lisäys fine";
+    }
+    else{
+        echo '<br />'."Jokin meni vikaan";
+    }
+    $stmt->close();
+    $connection->close();
+*/
 
 
-//$connection->close();
-
+ /*   //Kirjan poistaminen id:llä toimii
+    $sql= "DELETE FROM kirjat WHERE Kirja_Id=?";
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param("i",$kid);
+    $boolean =$stmt->execute();
+    if($boolean > 0){
+        echo '<br />'."Poisto onnistui";
+    }
+    else{
+        echo '<br />'."Jokin meni vikaan";
+    }
+    $stmt->close();
+    $connection->close();
+*/
 //
 //      Apumetodit
 //
-function getKirjaHakuKriteerit(){
+
+/*function getKirjaHakuKriteerit(){
     $nimi = $_GET["nimi"];
     $knimi = $_GET["knimi"];
     $id = $_GET["id"];
@@ -150,5 +181,5 @@ function poistettavaKirja(){
     } else if ($metodi=="DELETE") {
         $kirja = poistettavaKirja();
     }
-
+*/
 ?>
